9041da3d99f2e6b37d5102a61c85ce2a09b9c5f1
KernelOS <oloko9945@gmail.com>
2025-07-11 00:43:22 -0300

    models/sales resolvido

--- models/sale.js
+++ models/sale.js
@@ -1,13 +1,13 @@
 const mongoose = require('mongoose');
 
 const saleSchema = new mongoose.Schema({
-    clientName: String,
-    saleAmount: Number,
-    immediateDiscount: Number,
-    installments: Number,
-    installmentDiscount: Number,
-    finalAmount: Number,
-    paymentMethod: String
-});
+  clientName: String,
+  saleAmount: Number,
+  immediateDiscount: Number,
+  installments: Number,
+  installmentDiscount: Number,
+  paymentMethod: String,
+  finalAmount: Number,
+}, { timestamps: true });
 
 module.exports = mongoose.model('Sale', saleSchema);
